78cf20cd54c7d06e7860e8d0924a8d52a7e18e05
Ksenia Bataeva <kclosu@gmail.com>
2021-07-12 10:55:33 +0300

    run ci on every push

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kn15',
-    version='1.0.0',
+    version='2.0.0',
     description='a python package that parses coded КН-15 hydrology reports',
     packages=find_packages()
 )
